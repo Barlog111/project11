cb3e3710662e0c8e6e3faa32198c13dec37ab80a
Barlog111 <barlog2005@yandex.ru>
2022-08-08 10:19:45 +0300

    https://github.com/Barlog111/project11.git

---
+++ 123.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const add = (a, b) => a + b;
+
+console.log('Add numders: 5 + 2 = ' + add(5, 2));
+console.log('Add numders: 5.1 + 2.3 = ' + add(5.1, 2.3));
+console.log('Concatenate: \'5\' + \'2\' = ' + add('5', '2'));
+console.log('Subtraction: 5 + (-2) = ' + add(5, -2));
